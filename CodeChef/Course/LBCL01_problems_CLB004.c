c86dad3feb3f76a8ba2ac644ca8d0e7b819d9324
4F24L <108697331+4F24L@users.noreply.github.com>
2024-03-18 23:16:52 +0530

    learn c added

---
+++ CodeChef/Course/LBCL01_problems_CLB004.c
@@ -0,0 +1,24 @@
+// Arithmetic Operations
+// We can also perform mathematical operations (like addition, subtraction etc) with printf().
+
+// These use the familiar symbols of:
+
+// + for addition
+// − for subtracti
+// ∗ for multiplication
+// / for division
+// To add two number we do this:
+
+// printf("%d", 21 + 40);
+// The above line of code will output: 61
+
+// Again, printf is the function we use when we want to print. "%d" tells the compiler that we want to print a number. And writing 21 + 40 tells the compiler to add these two number and print the sum.
+
+#include <stdio.h>
+
+int main() {
+
+  // Replace the underscores
+  printf("%d", 21 + 40);
+
+}
---
+++ CodeChef/Course/LBCL02_problems_CLB007.c
@@ -0,0 +1,11 @@
+// Multiple Outputs
+// We can add as many printf() statements as we want in our code.
+
+#include <stdio.h>
+
+int main() {
+	
+	printf("%d", 3 + 4);
+	printf("%d", 2 + 1);
+	
+}
