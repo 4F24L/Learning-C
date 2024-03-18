45efdd89e1aa0d5711a707ec3ac5f01bbf92cfdf
4F24L <108697331+4F24L@users.noreply.github.com>
2024-03-18 23:04:24 +0530

    4 codechef ques

---
+++ CodeChef/Practice/PCL05_problems_FLOW001.c
@@ -0,0 +1,18 @@
+// Add Two Numbers
+// Your task is very simple: given two integers A and B, write a program to add these two numbers and output the sum.
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    // Loop for each test case
+    while (t--) {
+        int a, b;
+
+        scanf("%d %d", &a, &b);
+
+        printf("%d\n", a + b);
+    }
+}
